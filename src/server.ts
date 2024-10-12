bcebac9ce3e7e1bb87a291f86256f8b7b7127f37
Rishabh Dotasara <rishabhdotasara@gmail.com>
2024-10-12 23:36:25 +0530

    server fixes hopefully again

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"], // Replace with your frontend URL
+    origin: ["http://localhost:3000", "https://chess-iota-taupe.vercel.app/"], // Replace with your frontend URL
   },
 });
 
